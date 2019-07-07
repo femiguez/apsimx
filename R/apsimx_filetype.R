b88707a3589ee740ac69b8d753220625da28b1c7
Fernando Miguez <fer.eze.miguez@gmail.com>
2019-07-07 17:42:28 -0500

    Reorganizing functions and 'making room' to provide support for JSON files

---
+++ R/apsimx_filetype.R
@@ -0,0 +1,46 @@
+
+#' Test whether an .apsimx file is XML or json
+#' 
+#' @title Test file format for .apsimx files
+#' @name apsimx_filetype
+#' @param file file ending in .apsimx to be tested
+#' @param src.dir directory containing the .apsimx file to be tested; defaults to the current working directory
+#' @return 'xml', 'json' or 'unknown'
+#' @note Minimal function which reads only the first line in a file and tries to guess whether it is
+#'       an 'xml' or 'json' file type.
+#' @export
+#' @examples 
+#' \dontrun{
+#' ex.dir <- auto_detect_apsimx_examples()
+#' apsimx_filetype("Barley", src.dir = ex.dir) 
+#' apsimx_filetype("Chicory", src.dir = ex.dir) 
+#' apsimx_filetype("Oats", src.dir = ex.dir)                               
+#' apsimx_filetype("Maize", src.dir = ex.dir)
+#' apsimx_filetype("Sugarcane", src.dir = ex.dir)
+#' }
+#' 
+
+apsimx_filetype <- function(file = "", src.dir = "."){
+  
+  
+  fileNames <- dir(path = src.dir, pattern=".apsimx$",ignore.case=TRUE)
+  
+  if(length(fileNames)==0){
+    stop("There are no .apsimx files in the specified directory to test.")
+  }
+  
+  file <- match.arg(file, fileNames, several.ok=FALSE)
+  
+  rfl1 <- read.table(file = paste0(src.dir,"/",file), nrows = 1)[1,1]
+  ## The next line will work if all XML files contain xml in the first line
+  if(length(grep("xml", as.character(rfl1))) != 0){
+    ans <- "xml"
+  }else{
+    if(length(grep("{", as.character(rfl1), fixed = TRUE)) == 0){
+      ans <- "unkown"
+    }else{
+      ans <- "json"
+    }
+  }
+  return(ans)
+}
--- doc/apsimx.R
+++ doc/apsimx.R
@@ -17,20 +17,22 @@ inspect_apsimx("Maize", src.dir = ex.dir, node = "Weather")
 ## ----edit-apsimx---------------------------------------------------------
 ex.dir <- auto_detect_apsimx_examples()
 ocs <- c(1.5,1.4,1.3,1.2,1.1,1.0,0.9)
-edit_apsimx("Maize.apsimx", src.dir = ex.dir,
-            wrt.dir = ".",
-            node = "Soil",
-            soil.child = "OrganicMatter", 
-            parm = "OC", value = ocs)
+edit_apsimx_xml("Maize.apsimx", src.dir = ex.dir,
+                wrt.dir = ".",
+                node = "Soil",
+                soil.child = "OrganicMatter", 
+                parm = "OC", value = ocs)
 
-## ----edit-apsimx-delete, echo = FALSE------------------------------------
+## ----edit-apsimx-delete, echo = TRUE-------------------------------------
 ## delete the created file
 system("rm ./Maize-edited.apsimx")
 
 ## ----apsimx--------------------------------------------------------------
+## Detect 'Examples' directory
 ex.dir <- auto_detect_apsimx_examples()
+## Run simulation, this will take a while
 sim <- apsimx("Wheat.apsimx", src.dir = ex.dir, value = "report", cleanup = 1)
-## 
+## Calcualte summary statistics on all variables
 summary(sim)
 ## Plot data
 ggplot(data = sim, aes(x = Date, y = Yield)) + geom_point()
