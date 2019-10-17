ed8e6b1e8a0c2a6949aff820dd4a048d65bc4678
Fernando Miguez <fer.eze.miguez@gmail.com>
2019-10-16 20:42:00 -0500

    Adding 'test_inspect.R'

---
+++ tests/test_inspect.R
@@ -0,0 +1,77 @@
+## Run a few tests for the examples
+
+ex.dir <- auto_detect_apsimx_examples()
+
+## Run inspect tests
+
+run.inspect.tests <- FALSE
+
+if(run.inspect.tests){
+  
+  ## Barley
+  i <- "Barley.apsimx"
+  inspect_apsimx(i, src.dir = ex.dir, node = "Clock")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Weather")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "OrganicMatter")
+  ## inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Chemical") This does not work at the moment
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "InitialWater")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Sample")
+  inspect_apsimx(i, src.dir = ex.dir, node = "SurfaceOrganicMatter")
+  inspect_apsimx(i, src.dir = ex.dir, node = "MicroClimate")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Crop")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager", parm = list("SowingFertiliser",NA))
+
+  ## Maize
+  i <- "Maize.apsimx"
+  inspect_apsimx(i, src.dir = ex.dir, node = "Clock")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Weather")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "OrganicMatter")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Chemical")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "InitialWater")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Sample")
+  inspect_apsimx(i, src.dir = ex.dir, node = "SurfaceOrganicMatter")
+  inspect_apsimx(i, src.dir = ex.dir, node = "MicroClimate")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Crop")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager", parm = list('SowingRule',NA))
+  
+  ## Oats
+  i <- "Oats"
+  inspect_apsimx(i, src.dir = ex.dir, node = "Clock")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Weather")        
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "OrganicMatter")
+  ## inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Chemical") This does not work at the moment
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "InitialWater")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Soil", soil.child = "Sample")
+  inspect_apsimx(i, src.dir = ex.dir, node = "SurfaceOrganicMatter")
+  inspect_apsimx(i, src.dir = ex.dir, node = "MicroClimate")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Crop")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager")
+  inspect_apsimx(i, src.dir = ex.dir, node = "Manager", parm = list('SowingRule',NA))
+  
+  ## Chemical works fine for Maize, but not for Barley and Oats
+}
+
+inspect.replacement.test <- FALSE
+
+if(inspect.replacement.test){
+  
+  ## Define the highest level
+  inspect_apsimx_replacement("Factorial", src.dir = ex.dir, 
+                             root = list("Experiment", 1),
+                             node = "Base", node.child = "Clock")
+  
+  inspect_apsimx_replacement("Factorial", src.dir = ex.dir, 
+                             root = list("Experiment", 1),
+                             node = "Base", node.child = "Weather")
+  
+  inspect_apsimx_replacement("Factorial", src.dir = ex.dir, 
+                             root = list("Experiment", 1),
+                             node = "Base", node.child = "Field",
+                             node.subchild = "Soil",
+                             display.available = TRUE)
+}
