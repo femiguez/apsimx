d23ecfc6778dc82b928d3c589aed1255ec49343f
Fernando Miguez <fer.eze.miguez@gmail.com>
2019-11-25 11:13:01 -0600

    Adding new file to batch replace the soil profile

---
+++ R/edit_apsimx_replace_soil_profile.R
@@ -0,0 +1,146 @@
+#'
+#' @title Create APSIM-X Replace the Soil Profile in a simulation
+#' @name edit_apsimx_replace_soil_profile
+#' @description Edits an APSIM-X simulation by replacing the soil profile
+#' @param file name of the .apsimx file to be edited
+#' @param src.dir source directory
+#' @param wrt.dir writing directory
+#' @param soil.profile a soil profile object with class 'soil_profile'
+#' @param edit.tag default edit tag '-edited'
+#' @param overwrite default FALSE
+#' @return writes a file to disk with the supplied soil profile
+#' @details This function is designed to batch replace the whole soil in an APSIM simulation. 
+#' @note There is no such thing as a default soil, carefully build the profile for each simulation.
+#' @export
+#' @examples 
+#' \dontrun{
+#' sp <- apsimx_soil_profile()
+#' extd.dir <- system.file("extdata", package = "apsimx")
+#' edit_apsimx_replace_soil_profile("Maize.apsimx", soil.profile = sp, 
+#'                                   src.dir = extd.dir, wrt.dir = ".")
+#' inspect_apsimx("Maize-edited.apsimx", src.dir = ".",
+#'                  node = "Soil")
+#'  }
+#'
+
+edit_apsimx_replace_soil_profile <-  function(file = "", src.dir = ".",
+                                              wrt.dir = NULL,
+                                              soil.profile = NULL,
+                                              edit.tag = "-edited",
+                                              overwrite = FALSE,
+                                              verbose = TRUE){
+  
+  check_apsim_name(file)
+  
+  if(missing(wrt.dir)) wrt.dir <- src.dir
+  
+  file.names <- dir(path = src.dir, pattern=".apsimx$",ignore.case=TRUE)
+  
+  if(length(file.names)==0){
+    stop("There are no .apsimx files in the specified directory to edit.")
+  }
+  
+  file <- match.arg(file, file.names)
+  
+  if(missing(soil.profile)) stop("soil profile is missing")
+  
+  ## Parse apsimx file (JSON)
+  apsimx_json <- read_json(paste0(src.dir,"/",file))
+  
+  parent.node0 <- apsimx_json$Children
+  ## Where is the 'Core' simulation?
+  wcore <- grep("Core.Simulation", parent.node0)
+  parent.node <- parent.node0[[wcore]]$Children
+  
+  ## Extract 'Core' simulation
+  wcz <- grepl("Models.Core.Zone", parent.node)
+  core.zone.node <- parent.node[wcz][[1]]$Children
+  
+  ## Extract soil
+  wsn <- grepl("Models.Soils.Soil", core.zone.node)
+  soil.node <- core.zone.node[wsn]
+  
+  soil.node0 <- soil.node[[1]]$Children
+  
+  ## First edit: soil 'Water' or 'Physical'
+  ## Depth, Thickness, ParticleSizeClay,
+  ## BD, AirDry, LL15, DUL, SAT, KS
+  soil.physical.node <- soil.node[[1]]$Children[[1]]
+  for(i in c("Depth","Thickness","ParticleSizeClay","BD","AirDry","LL15","DUL","SAT","KS")){
+    ## Format the variable
+    if(i == "ParticleSizeClay") next
+    tmp <- as.vector(soil.profile$soil[[i]], mode = "list")
+    ## Replace the variable
+    soil.physical.node[[i]] <- tmp 
+  }
+  ## Preliminary setup of soil
+  if(length(soil.profile$crops) > length(soil.physical.node$Children)){
+    for(i in seq_along(soil.profile$crops)){
+      soil.physical.node$Children[[i]] <- soil.physical.node$Children[[1]]
+      soil.physical.node$Children[[i]]$Name <- paste0(soil.profile$crops[i],"Soil")
+    }
+  }
+  ## Crop parameters
+  for(i in 1:length(soil.profile$crops)){
+    for(j in c("crop.XF","crop.KL","crop.LL")){
+      tmp <- as.vector(soil.profile$soil[[j]], mode = "list")
+      strpcrop <- strsplit(j,".",fixed = TRUE)[[1]][2]
+      soil.physical.node$Children[[i]][[strpcrop]] <- tmp
+    }
+  }
+  soil.node[[1]]$Children[[1]] <- soil.physical.node
+  soil.node0 <- soil.node[[1]]$Children
+  
+  ## Next edit the soil organic component
+  wsomn <- grepl("Organic", soil.node0)
+  soil.om.node <- soil.node0[wsomn][[1]]
+  
+  for(i in c("Depth","Thickness","Carbon","SoilCNRatio","FBiom","FInert","FOM")){
+    ## Format the variable
+    tmp <- as.vector(soil.profile$soil[[i]], mode = "list")
+    ## Replace the variable
+    soil.om.node[[i]] <- tmp 
+  }
+  soil.node0[wsomn][[1]] <- soil.om.node
+  soil.node[[1]]$Children <- soil.node0
+  
+  ## Next edit the Chemical component
+  wschn <- grepl("Chemical", soil.node0)
+  soil.chemical.node <- soil.node0[wschn][[1]]
+  
+  for(i in c("Depth","Thickness","NO3N","NH4N","PH")){
+    ## Format the variable
+    tmp <- as.vector(soil.profile$soil[[i]], mode = "list")
+    ## Replace the variable
+    soil.chemical.node[[i]] <- tmp 
+  }
+  soil.node0[wschn][[1]] <- soil.chemical.node
+  soil.node[[1]]$Children <- soil.node0
+  
+  ## Replace the soil
+  core.zone.node[wsn] <- soil.node
+  
+  parent.node[wcz][[1]]$Children <- core.zone.node
+  apsimx_json$Children[[1]]$Children <- parent.node
+  
+  if(overwrite == FALSE){
+    wr.path <- paste0(wrt.dir,"/",
+                      strsplit(file,".",fixed = TRUE)[[1]][1],
+                      edit.tag,".apsimx")
+  }else{
+    wr.path <- paste0(wrt.dir,"/",file)
+  }
+  
+  write_json(apsimx_json, path = wr.path, 
+             pretty = TRUE, digits = NA, 
+             auto_unbox = TRUE, null = "null")
+  
+  if(verbose){
+    ##cat("Edited (node): ",node, "\n")
+    ##cat("Edited (child): ", edited.child,"\n")
+    ##cat("Edited parameter: ",parm, "\n")
+    ##cat("New values: ",value, "\n")
+    cat("Created: ",wr.path,"\n")
+  }
+  
+}
