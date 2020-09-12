cbb5692520d03ab523a36543f9f5cd4933f61a5d
Fernando Miguez <fer.eze.miguez@gmail.com>
2020-09-12 17:02:45 -0500

    Adding details to the vignette about parameter optimization

--- doc/apsimx.R
+++ doc/apsimx.R
@@ -18,7 +18,8 @@ tmp.dir <- tempdir()
 
 ## ----read-in-maize-example, eval = TRUE, echo = FALSE--------------------
 extd.dir <- system.file("extdata", package = "apsimx")
-maize <- read_apsimx("Maize.db", src.dir = extd.dir)
+## maize <- read_apsimx("Maize.db", src.dir = extd.dir)
+maize <- read.csv(paste0(extd.dir, "/Maize.csv"))
 
 ## ----summary-ggplot-maize------------------------------------------------
 summary(maize)
@@ -31,21 +32,21 @@ extd.dir <- system.file("extdata", package = "apsimx")
 inspect_apsimx("Maize.apsimx", src.dir = extd.dir, node = "Weather")
 
 ## ----inspect-maize-oc----------------------------------------------------
-inspect_apsimx("Maize.apsimx", src.dir = extd.dir, node = "Soil",
-               soil.child = "Organic")
+inspect_apsimx("Maize.apsimx", src.dir = extd.dir, 
+               node = "Soil", soil.child = "Organic")
 
 ## ----edit-maize-oc-------------------------------------------------------
-ocs <- c(1.5,1.4,1.3,1.2,1.1,1.0,0.9)
-edit_apsimx("Maize.apsimx", src.dir = extd.dir,
-             wrt.dir = ".",
-             node = "Soil",
-             soil.child = "Organic", 
-             parm = "Carbon", value = ocs)
+ocs <- c(1.5, 1.4, 1.3, 1.2, 1.1, 1.0, 0.9)
+edit_apsimx("Maize.apsimx", 
+            src.dir = extd.dir,
+            wrt.dir = ".",
+            node = "Soil",
+            soil.child = "Organic", 
+            parm = "Carbon", value = ocs)
 
 ## ----inspect-maize-oc-check----------------------------------------------
 inspect_apsimx("Maize-edited.apsimx", src.dir = ".", 
-               node = "Soil",
-               soil.child = "Organic")
+               node = "Soil", soil.child = "Organic")
 
 ## ----apsimx-wheat-no-run, eval = FALSE-----------------------------------
 #  sim <- apsimx("Wheat.apsimx", src.dir = extd.dir, value = "report")
@@ -55,11 +56,12 @@ inspect_apsimx("Maize-edited.apsimx", src.dir = ".",
 #  ## Copy 'Wheat' file to a temporary directory
 #  ## (or change as needed)
 #  tmp.dir2 <- tempdir()
-#  file.copy(paste0(ex.dir,"/","Wheat.apsimx"), tmp.dir2)
+#  file.copy(paste0(ex.dir, "/", "Wheat.apsimx"), tmp.dir2)
 #  sim <- apsimx("Wheat.apsimx", src.dir = tmp.dir2, value = "report")
 
 ## ----apsimx-wheat-read, echo = FALSE-------------------------------------
-sim <- read_apsimx("Wheat.db", src.dir = extd.dir)
+## sim <- read_apsimx("Wheat.db", src.dir = extd.dir)
+sim <- read.csv(paste0(extd.dir, "/Wheat.csv"))
 
 ## ----apsimx-wheat-summary------------------------------------------------
 ## Calcualte summary statistics on all variables
@@ -72,14 +74,14 @@ inspect_apsimx("Wheat.apsimx", src.dir = extd.dir, node = "Crop")
 inspect_apsimx("Wheat.apsimx", src.dir = extd.dir, node = "Manager")
 ## Looking more in-depth into 'SowingRule1'
 inspect_apsimx("Wheat.apsimx", src.dir = extd.dir, 
-               node = "Manager", parm = list("SowingRule1",NA))
+               node = "Manager", parm = list("SowingRule1", NA))
 
 ## ----apsimx-wheat-cultivar-only------------------------------------------
 inspect_apsimx("Wheat.apsimx", src.dir = extd.dir, 
-               node = "Manager", parm = list("SowingRule1",6))
+               node = "Manager", parm = list("SowingRule1", 6))
 ## We can print and store the path to this parameter
 pp <- inspect_apsimx("Wheat.apsimx", src.dir = extd.dir, 
-               node = "Manager", parm = list("SowingRule1",6),
+               node = "Manager", parm = list("SowingRule1", 6),
                print.path = TRUE)
 
 ## ----Millet--------------------------------------------------------------
@@ -87,13 +89,13 @@ inspect_apsim("Millet.apsim", src.dir = extd.dir, node  = "Manager")
 
 ## ----Millet-sow----------------------------------------------------------
 inspect_apsim("Millet.apsim", src.dir = extd.dir, node  = "Manager",
-              parm = list("Sow on a fixed date",NA))
+              parm = list("Sow on a fixed date", NA))
 
 ## ----Millet-sow-plt-dens-------------------------------------------------
 inspect_apsim("Millet.apsim", src.dir = extd.dir, node  = "Manager",
               parm = list("Sow on a fixed date",5), print.path = TRUE)
 ## Or store it in an object for later editing
-pp <- inspect_apsim("Millet.apsim", src.dir = extd.dir, node  = "Manager",
+pp <- inspect_apsim("Millet.apsim", src.dir = extd.dir, node = "Manager",
               parm = list("Sow on a fixed date",5), print.path = TRUE)
 
 ## ----Millet-edit---------------------------------------------------------
@@ -104,11 +106,11 @@ edit_apsim("Millet.apsim", src.dir = extd.dir, wrt.dir = tmp.dir,
 ## ----Millet-edit-inspect-------------------------------------------------
 inspect_apsim("Millet-pp.apsim", src.dir = tmp.dir,
               node = "Manager",
-              parm = list("Sow on a fixed date",NA))
+              parm = list("Sow on a fixed date", NA))
 
 ## ----removing-Millet-pp, echo = FALSE, eval = TRUE-----------------------
 ## Apparently this is not needed
-file.remove(paste0(tmp.dir,"/Millet-pp.apsim"))
+file.remove(paste0(tmp.dir, "/Millet-pp.apsim"))
 
 ## ----inspect-replacement-node--------------------------------------------
 inspect_apsimx_replacement("MaizeSoybean.apsimx", src.dir = extd.dir,
@@ -145,14 +147,15 @@ inspect_apsimx_replacement("MaizeSoybean.apsimx", src.dir = extd.dir,
                            node.subchild = "ThermalTime", 
                            node.subsubchild = "BaseThermalTime",
                            node.sub3child = "TemperatureResponse",
-                           parm = c("Y","X")) 
+                           parm = c("Y", "X")) 
 
 ## ----inspect-replacement-soybean-cultivar-node---------------------------
 inspect_apsimx_replacement("MaizeSoybean.apsimx", src.dir = extd.dir,
                            node = "Soybean", display.available = TRUE) 
 
 ## ----inspect-replacement-soybean-cultivar-node-child---------------------
-inspect_apsimx_replacement("MaizeSoybean.apsimx", src.dir = extd.dir,
+inspect_apsimx_replacement("MaizeSoybean.apsimx", 
+                           src.dir = extd.dir,
                            node = "Soybean", 
                            node.child = "PioneerP22T61_MG22",
                            display.available = FALSE) 
@@ -164,7 +167,7 @@ edit_apsimx_replacement("MaizeSoybean.apsimx",
                         node.subchild = "ThermalTime", 
                         node.subsubchild = "BaseThermalTime",
                         node.sub3child = "TemperatureResponse",
-                        parm = "Y", value = c(0,12,20,28,0))
+                        parm = "Y", value = c(0, 12, 20, 28, 0))
 
 ## ----inspect-edit-replacement--------------------------------------------
 inspect_apsimx_replacement("MaizeSoybean-edited.apsimx", 
@@ -179,21 +182,21 @@ inspect_apsimx_replacement("MaizeSoybean-edited.apsimx",
 ## ----inspect-factorial-0, eval = FALSE-----------------------------------
 #  ## There are multiple 'Experiments' so we need to pick one
 #  inspect_apsimx_replacement("Factorial", src.dir = extd.dir,
-#                             root = list("Experiment",NA))
+#                             root = list("Experiment", NA))
 #  ##These positions matched  Experiment   1 2 3 4 5
 #  ##Error in inspect_apsimx_replacement("Factorial", src.dir = ex.dir, root = ##list("Experiment", : Multiple root nodes found. Please provide a position
 
 ## ----inspect-factorial---------------------------------------------------
 ## There are multiple 'Experiments' so we need to pick one
 inspect_apsimx_replacement("Factorial", src.dir = extd.dir,
-                           root = list("Experiment",1))
+                           root = list("Experiment", 1))
 ## We need to provide a node
 inspect_apsimx_replacement("Factorial", src.dir = extd.dir,
-                           root = list("Experiment",1), 
+                           root = list("Experiment", 1), 
                            node = "Base", display.available = TRUE)
 ## We need to provide a node child
 inspect_apsimx_replacement("Factorial", src.dir = extd.dir,
-                           root = list("Experiment",1), 
+                           root = list("Experiment", 1), 
                            node = "Base", node.child = "Clock",
                            display.available = TRUE)
 
@@ -212,17 +215,23 @@ inspect_apsimx_replacement("Factorial", src.dir = extd.dir,
 
 ## ----inspect-apsimx-factorial-2------------------------------------------
 inspect_apsimx("Factorial.apsimx", src.dir = extd.dir,
-               root = c("RangeExperiment","Base2"),
+               root = c("RangeExperiment", "Base2"),
                node = "Weather")
 
 ## ----edit-apsimx-factorial-2---------------------------------------------
 edit_apsimx("Factorial.apsimx", 
             src.dir = extd.dir, wrt.dir = tmp.dir,
-            root = c("RangeExperiment","Base2"),
+            root = c("RangeExperiment", "Base2"),
             node = "Weather", 
             value = "Ames.met")
 
 ## ----apsim-verions-tail, eval = FALSE------------------------------------
 #  ava <- apsim_version()
 #  aiu <- apsim_version(which = "inuse")
+
+## ----inspect-apsimx-xml-maize--------------------------------------------
+rue.pth <- inspect_apsim_xml("Maize75.xml", src.dir = extd.dir, parm = "rue")
+ext.pth <- inspect_apsim_xml("Maize75.xml", src.dir = extd.dir, parm = "y_extinct_coef")
+## To pass them to optim_apsim, combine them
+pp <- c(rue.pth, ext.pth)
 
