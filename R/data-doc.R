f74bf09934297049c2156aefad107a2539be3c6a
Fernando Miguez <fer.eze.miguez@gmail.com>
2020-09-15 19:23:23 -0500

    Adding material for optimization vignette

---
+++ R/data-doc.R
@@ -0,0 +1,14 @@
+#' Artificial observed data for Wheat
+#'
+#' A dataset containing the Date, phenology stage, LAI and above ground biomass for Wheat
+#'
+#' @title Observed wheat phenology, LAI and biomass
+#' @format A data frame with 10 rows and 4 variables:
+#' \describe{
+#'   \item{Date}{ -date- date starting Oct 1 2016 and ending June 6 2017}
+#'   \item{Wheat.Phenology.Stage}{ -numeric- phenology stage of wheat}
+#'   \item{Wheat.Leaf.LAI}{ -numeric- Leaf Area Index}
+#'   \item{Wheat.AboveGround.wt}{-numeric- above ground biomass (g/m2)}
+#' }
+#' @source This are simulated data. For details see the APSIM documentation
+"obsWheat"
