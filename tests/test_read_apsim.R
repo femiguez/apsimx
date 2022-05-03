8d12a7ea66379451ec92b6c92ba11738f8a90c43
Fernando Miguez <fer.eze.miguez@gmail.com>
2022-05-03 16:14:14 -0500

    Adding the possibility of selecting specific outputfiles when using APSIM classic

--- tests/test_read_apsim.R
+++ tests/test_read_apsim.R
@@ -1,8 +1,37 @@
 ## Testing read_apsim_all function
 require(apsimx)
 
+run.examples <- FALSE
+
 extd.dir <- system.file("extdata", package = "apsimx")
 
 maize.out <- read_apsim("Maize.out", src.dir = extd.dir)
 
 millet.out <- read_apsim("Millet.out", src.dir = extd.dir)
+
+## Running all examples
+os <- .Platform$OS.type
+
+if(os == "windows" && run.examples){
+  
+  apsim_options(warn.versions = FALSE)
+  ex.classic.dir <- apsimx:::auto_detect_apsim_examples()
+  apsim.examples <- dir(ex.classic.dir, pattern = "apsim$")
+  
+  tmp.dir <- tempdir()
+  setwd(tmp.dir)
+  
+  for(i in apsim.examples){
+    if(grepl("\\s", i)) next
+    file.copy(file.path(ex.classic.dir, i), to = file.path(tmp.dir, i))
+    cat("Example:", i)
+    sim <- try(apsim(file = i, simplify = FALSE), silent = TRUE)
+    if(inherits(sim, "try-error")){
+      cat(" Failed. \n")
+      next
+    }else{
+      cat("; object class:", class(sim), " ")
+      cat("; Done. \n")  
+    }
+  }
+}
